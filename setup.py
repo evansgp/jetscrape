96500684a80fd829a5d0267b1d8eedc097945df6
Gareth Evans <evans.g.p@gmail.com>
2017-02-15 21:49:58 +0800

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='jetscrape',
+      version='0.1',
+      description='Download Jetstar (AU) CC data',
+      url='http://github.com/evansgp/jetscrape',
+      author='Gareth Evans',
+      author_email='evans.g.p@gmail.com',
+      packages=['jetscrape'],
+      zip_safe=False)
