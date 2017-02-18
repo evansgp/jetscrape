b24fac31d2f7c377c66446218fbc2b8310401ab9
Gareth Evans <evans.g.p@gmail.com>
2017-02-18 08:46:42 +0800

    call API from entry_point

--- jetscrape/command_line.py
+++ jetscrape/command_line.py
@@ -1,2 +1,25 @@
+from jetscrape import Configuration, Account
+import logging
+import argparse
+
+
+logger = logging.getLogger(__name__)
+
+
 def main():
-    print('test')
+    args = process_command_line()
+    logging.basicConfig(level=args.level)
+    Configuration.configure(args.username, args.password)
+    for account in Account.list():
+        print(account.data)
+    return 0
+
+
+def process_command_line():
+    parser = argparse.ArgumentParser(description='Download Jetstar (AU) CC data')
+    parser.add_argument('-u', '--username', required=True, help='username to connect with')
+    parser.add_argument('-p', '--password', required=True, help='password to connect with')
+    parser.add_argument('-v', dest='level', action='store_const', const=logging.DEBUG, default=logging.INFO,
+                        help='verbose output')
+    return parser.parse_args()
+
---
+++ jetscrape/tests/test_command_line.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+from jetscrape.command_line import main
+
+
+class TestJoke(TestCase):
+    def test_main(self):
+        main()
