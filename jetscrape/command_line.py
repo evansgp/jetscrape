4dc0660eed5938fa4c63f161d8158b7c73a34691
Gareth Evans <evans.g.p@gmail.com>
2017-02-19 07:35:17 +0800

    output to csv

--- jetscrape/command_line.py
+++ jetscrape/command_line.py
@@ -2,6 +2,9 @@ import argparse
 import logging
 import jetscrape
 import jsonclient
+import csv
+import sys
+import datetime
 
 logger = logging.getLogger(__name__)
 
@@ -9,14 +12,29 @@ logger = logging.getLogger(__name__)
 def main():
     args = process_command_line()
     logging.basicConfig(level=args.level)
-    logger.debug('running with arguements: %s', args)
     jsonclient.authenticator = jetscrape.auth(args.username, args.password)
     jsonclient.pager = jetscrape.pager
-    for account in jetscrape.Account.list():
-        logger.info('got account: %s', account)
-        for transaction in account.transactions:
-            logger.info('got transaction: {txn.date} {txn.description} {txn.amount} {txn.debit}'.format(txn=transaction))
+    target = datetime.date.today() - datetime.timedelta(days=args.days)
+    logger.info('target date is {}'.format(target))
+    accounts = jetscrape.Account.list()
+    writer = csv.writer(args.file)
+    for account in accounts:
+        for txn in account.transactions:
+            if txn.date < target:
+                logger.info('hit target with {}'.format(txn.date))
+                break
+            writer.writerow(to_row(txn))
+
     return 0
+
+
+def to_row(txn):
+    return [
+        txn.date.strftime('%d %b %Y'),
+        txn.description,
+        "DR" if txn.debit else "CR",
+        txn.amount
+    ]
 
 
 def process_command_line():
@@ -25,5 +43,7 @@ def process_command_line():
                         help='verbose output')
     parser.add_argument('-u', '--username', required=True, help='username to connect with')
     parser.add_argument('-p', '--password', required=True, help='password to connect with')
+    parser.add_argument('-d', '--days', default=31, help='the number of days to process')
+    parser.add_argument('-f', '--file', default=sys.stdout, type=argparse.FileType('w'), help='where to direct output')
     return parser.parse_args()
 
